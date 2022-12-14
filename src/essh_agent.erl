def72eca010dbc2e62b3943a804e5ba0167695c1
Stefan Grundmann <sg2342@googlemail.com>
2022-12-14 00:27:48 +0000

    add authentication agent escript

--- src/essh_agent.erl
+++ src/essh_agent.erl
@@ -2,44 +2,79 @@
 
 -export([main/1]).
 
-
 main([]) -> main([sockpath()]);
 main([SockPath]) ->
     {ok, _} = application:ensure_all_started(public_key),
-    {ok, Pid} = essh_agents:start_link([{confirm, fun confirm/1}]),
-    listen(SockPath, Pid).
+    {ok, Agent} = essh_agents:start_link([{confirm, fun confirm/1}]),
+    MaybeLSock = gen_tcp:listen(0,[{ifaddr,{local, SockPath}}, local, binary,
+				  {packet, 4}, {active, once}]),
+listen(MaybeLSock, #{sock_path => SockPath, agent => Agent}).
 
 
-listen(SockPath, Agent) ->
-    logger:notice("SSH_AUTH_SOCK="++SockPath),
-    listen1(gen_tcp:listen(0,[{ifaddr,{local, SockPath}}, local, binary,
-			      {packet, 4}, {active, once}]), Agent).
-
-listen1({error, _} = E, _Agent) ->
+listen({error, _} = E, _M) ->
     logger:error("listen failed: ~p",[E]);
-listen1({ok, LSock}, Agent) ->
+listen({ok, LSock}, #{sock_path := SockPath} = M) ->
+    logger:notice("SSH_AUTH_SOCK="++SockPath),
+    ok = file:change_mode(SockPath, 8#600),
+    SignalServer = erlang:whereis(erl_signal_server),
+    erlang:unregister(erl_signal_server),
+    erlang:register(erl_signal_server, self()),
+    Signals =[sighup, sigquit, sigabrt, sigalrm, sigterm, sigusr1, sigusr2,
+	      sigchld, sigstop, sigtstp],
+    [os:set_signal(S, handle)||S <- Signals],
     erlang:process_flag(trap_exit, true),
+    listen_loop(M#{lsock => LSock, signal_server => SignalServer}).
+
+
+listen_loop(#{lsock := LSock, agent := Agent} = M) ->
     Listener = self(),
-    {Pid, Ref} =
+    {Connection, Ref} =
 	spawn_opt(
-	  fun() ->
-		  {ok, Sock} = gen_tcp:accept(LSock),
-		  Listener ! {self(), ok},
-		  connection(Sock, Agent)
-	  end, [monitor]),
+	  fun() -> connection(gen_tcp:accept(LSock), Listener, Agent) end,
+	  [monitor]),
     receive
-	{'DOWN', Ref, process, Reason} -> logger:error("Crash: ~p", Reason);
-	{Pid, ok} -> erlang:demonitor(Ref, [flush])
-    end,
-    listen1({ok, LSock}, Agent).
+	{'DOWN', Ref, process, Reason} ->
+	    logger:error("Connection DOWN: ~p", [Reason]),
+	    listen_loop(M);
+	{Connection, ok} ->
+	    erlang:demonitor(Ref, [flush]),
+	    listen_loop(M);
+	{'EXIT', Agent, Reason} ->
+	    logger:error("Agent EXIT: ~p", [Reason]),
+	    exit(Connection, kill),
+	    cleanup(M);
+	{'EXIT', _, Reason} ->
+	    logger:notice("Listener EXIT: ~p", [Reason]),
+	    exit(Connection,kill),
+	    cleanup(M);
+	{notify, Signal} ->
+	    exit(Connection, kill),
+	    logger:notice("received signal: ~p", [Signal]),
+	    cleanup(M),
+	    erl_signal_server ! {notify, Signal}
+    end.
 
 
-connection(S, Agent) ->
+cleanup(#{lsock := LSock, sock_path := SockPath,
+	  signal_server := SignalServer}) ->
+    gen_tcp:close(LSock),
+    _ = file:delete(SockPath),
+    erlang:unregister(erl_signal_server),
+    erlang:register(erl_signal_server, SignalServer).
+
+
+connection({ok, Sock}, Listener, Agent) ->
+    Listener ! {self(), ok},
+    connection_loop(Sock, Agent);
+connection(_, _, _) -> ok.
+
+
+connection_loop(S, Agent) ->
     receive
 	{tcp, S, Req} ->
 	    ok = gen_tcp:send(S, essh_agents:req(Agent, Req)),
 	    ok = inet:setopts(S, [{active, once}]),
-	    connection(S, Agent);
+	    connection_loop(S, Agent);
 	{tcp_closed, S} -> ok;
 	{tcp_error, S, _E} -> gen_tcp:close(S)
     end.
